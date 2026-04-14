b06129ba2d1a2da4f4647e0d874c5000a2a7266e
Stefanos Stamatiadis <stamatiad.st@gmail.com>
2026-04-14 12:02:31 +0300

    Compiles and runs on my Win machine.
    > Please double check I have included all the cmake src files.

---
+++ build.bat
@@ -0,0 +1,7 @@
+cmake ^
+-S C:/Users/stama/OneDrive/Documents/GitHub/LibRaw ^
+-B C:/Users/stama/OneDrive/Documents/GitHub/LibRaw/build ^
+-D CMAKE_INSTALL_PREFIX=C:/Users/stama/miniconda3/envs/amechanos
+::cmake --build build --config Release
+::cmake --install build
+	
